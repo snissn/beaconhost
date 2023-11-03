710e6a9bf2291e88fd391197981c4d181404d9eb
mike seiler <snissn@gmail.com>
2023-11-03 11:10:42 -0700

    init

---
+++ src/interfaces/IDiamond.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+interface IDiamond {
+    // Add=0, Replace=1, Remove=2
+    enum FacetCutAction {
+        Add,
+        Replace,
+        Remove
+    }
+
+    struct FacetCut {
+        address facetAddress;
+        FacetCutAction action;
+        bytes4[] functionSelectors;
+    }
+    // The DiamondCut event records all function changes to a diamond.
+    event DiamondCut(FacetCut[] _diamondCut, address _init, bytes _calldata);
+}

7294128c11def83d113f63394ebe6f766c86db84
mike seiler <snissn@gmail.com>
2023-11-04 18:02:22 -1000

    add file

---
+++ src/BeaconhostDiamond.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+import {LibDiamond} from "./lib/LibDiamond.sol";
+
+error FunctionNotFound(bytes4 _functionSelector);
+
+struct BeaconhostStorage {
+    //all storage objects go here! we can use structs and tokenhost style getters
+    mapping(bytes32 => address) storage;
+}
+
+
+
+
+contract BeaconhostDiamond {
+
+    BeaconhostStorage internal s;
+
+    constructor(IDiamond.FacetCut[] memory _diamondCut) {
+        LibDiamond.setContractOwner(msg.sender);
+        LibDiamond.diamondCut({_diamondCut: _diamondCut, _init: address(0), _calldata: new bytes(0)});
+    }
+
+    function _fallback() internal {
+        LibDiamond.DiamondStorage storage ds;
+        bytes32 position = LibDiamond.DIAMOND_STORAGE_POSITION;
+        // get diamond storage
+        // slither-disable-next-line assembly
+        assembly {
+            ds.slot := position
+        }
+        // get facet from function selector
+        address facet = ds.facetAddressAndSelectorPosition[msg.sig].facetAddress;
+        if (facet == address(0)) {
+            revert FunctionNotFound(msg.sig);
+        }
+        // Execute external function from facet using delegatecall and return any value.
+        // slither-disable-next-line assembly
+        assembly {
+            // copy function selector and any arguments
+            calldatacopy(0, 0, calldatasize())
+            // execute function call using the facet
+            let result := delegatecall(gas(), facet, 0, calldatasize(), 0, 0)
+            // get any return value
+            returndatacopy(0, 0, returndatasize())
+            // return any return value or error back to the caller
+            switch result
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
+        }
+    }
+
+    /// @notice Will run when no functions matches call data
+    fallback() external payable {
+        _fallback();
+    }
+
+    /// @notice Same as fallback but called when calldata is empty
+    receive() external payable {
+        _fallback();
+    }
+}
